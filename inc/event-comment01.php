f6bb61c197a45137f98c6c52ceab42f881054a04
delaymania <worldchord@gmail.com>
2024-09-06 14:05:14 +0900

    iframeのURL末尾にパラメーターを付与

--- inc/event-comment01.php
+++ inc/event-comment01.php
@@ -149,7 +149,7 @@
         -->
         
 				<div class="iframe-container">
-					<iframe id="iframeEvent" src="https://info.fuji-realty.com/l/1063212/2024-05-24/dz4ttw" width="100%" height="2200px" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe>
+					<iframe id="iframeEvent" src="https://info.fuji-realty.com/l/1063212/2024-05-24/dz4ttw?EventName=EventName__c" width="100%" height="2200px" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe>
 				</div>
 
 		    </div>
