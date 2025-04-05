aa446be9d528a8b482a6b35875b03223e90b55ae
delaymania <worldchord@gmail.com>
2025-04-05 10:41:57 +0900

    施工事例ページのカテゴリー並べ替え

--- page-project.php
+++ page-project.php
@@ -80,52 +80,7 @@ wp_reset_postdata();  // クエリをリセット
 ?>
 
   </section>
-
-<section class="project-section">
-    <h2 class="project-title">カリフォルニアステージ (自社ブランド)</h2>
-
-<?php
-// カスタムタクソノミーのタームを指定
-$term_slug = 'cali-stage-brand';
-$args = array(
-    'post_type' => 'project',
-    'posts_per_page' => -1,  // 全ての投稿を表示
-    'orderby' => 'date',     // 日付で並び替え
-    'order' => 'ASC',        // 古い順
-    'tax_query' => array(
-        array(
-            'taxonomy' => 'project-filter',
-            'field'    => 'slug',
-            'terms'    => $term_slug
-        )
-    )
-);
-
-$team_query = new WP_Query($args);
-
-// クエリが投稿を持っているか確認
-if ($team_query->have_posts()): ?>
-        
-    <div class="project-frame">
-      <?php while ($team_query->have_posts()): $team_query->the_post(); ?>
-      <div class="project-box">
-        <a href="<?php echo esc_url(get_permalink()); ?>">
-          <p class="ph_flame">
-            <?php if (has_post_thumbnail()): ?>
-              <img class="team_pic" src="<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(), 'thumb')); ?>" alt="<?php echo esc_attr(get_field('position')); ?>">
-            <?php endif; ?>
-          </p>
-          <div class="link_box"><h3 class="ttl"><?php the_title(); ?></h3></div>
-        </a>
-      </div>
-      <?php endwhile; ?>
-    </div>
-<?php endif;
-
-wp_reset_postdata();  // クエリをリセット
-?>
-
-  </section>
+  
   
 <section class="project-section">
     <h2 class="project-title">Custom House(注文住宅)</h2>
@@ -173,7 +128,53 @@ wp_reset_postdata();  // クエリをリセット
 ?>
 
 </section>
+  
 
+<section class="project-section">
+    <h2 class="project-title">カリフォルニアステージ (自社ブランド)</h2>
+
+<?php
+// カスタムタクソノミーのタームを指定
+$term_slug = 'cali-stage-brand';
+$args = array(
+    'post_type' => 'project',
+    'posts_per_page' => -1,  // 全ての投稿を表示
+    'orderby' => 'date',     // 日付で並び替え
+    'order' => 'ASC',        // 古い順
+    'tax_query' => array(
+        array(
+            'taxonomy' => 'project-filter',
+            'field'    => 'slug',
+            'terms'    => $term_slug
+        )
+    )
+);
+
+$team_query = new WP_Query($args);
+
+// クエリが投稿を持っているか確認
+if ($team_query->have_posts()): ?>
+        
+    <div class="project-frame">
+      <?php while ($team_query->have_posts()): $team_query->the_post(); ?>
+      <div class="project-box">
+        <a href="<?php echo esc_url(get_permalink()); ?>">
+          <p class="ph_flame">
+            <?php if (has_post_thumbnail()): ?>
+              <img class="team_pic" src="<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(), 'thumb')); ?>" alt="<?php echo esc_attr(get_field('position')); ?>">
+            <?php endif; ?>
+          </p>
+          <div class="link_box"><h3 class="ttl"><?php the_title(); ?></h3></div>
+        </a>
+      </div>
+      <?php endwhile; ?>
+    </div>
+<?php endif;
+
+wp_reset_postdata();  // クエリをリセット
+?>
+
+  </section>
   
   
 <section class="project-section">
