be22293727e5faa0005529d54f4ec51d87a131a1
delaymania <worldchord@gmail.com>
2025-10-19 18:04:14 +0900

    single-hello_zero-cube.phpの更新作業

--- single-hello_zero-cube.php
+++ single-hello_zero-cube.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
+	<main id="main" class="site-main zero-cube-muji" role="main">
 		
 		<!--<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/reset.css" type="text/css" media="all" />-->
 		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/layout_lineup.css" type="text/css" media="all" />
@@ -52,7 +52,7 @@
 		</div>
 		<div class="evetextbox">
 			<div class="under_contents">
-				<p class="txt ffa">ベースのZERO-CUBEに、+FUNで屋上バルコニーと1部屋、ウッドデッキをプラスしたBOX SKYBALCONY。<br><span class="ffb">家族のだんらんがたくさん詰まったオススメのFUN</span>を実現しています。</p>
+        <p class="txt ffa">ベースのZERO-CUBEに、+FUNで屋上バルコニーをプラスしたSKY BALCONY。<br><span class="ffb">家族のだんらんがたくさん詰まったオススメのFUN</span>を実現しています。</p>
 			</div>
 		</div>
 		<div class="eveplanbox">
@@ -60,16 +60,16 @@
 				<h4 class="titstyleb ffc">PLAN</h4>
 				<div class="eveplanin zero-cube">
 					<ul class="cf">
-						<li class="hlg01"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_eveplan01.gif" alt="ZERO-CUBE+BOX SKYBALCONY間取り1F"><span class="ffc">1F</span></li>
-						<li class="hlg01"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_eveplan02.gif" alt="ZERO-CUBE+BOX SKYBALCONY間取り2F"><span class="ffc">2F</span></li>
-						<li class="hlg01"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_eveplan03.gif" alt="ZERO-CUBE+BOX SKYBALCONY間取り3F"><span class="ffc">3F</span></li>
+						<li class="hlg01"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_eveplan01.jpg" alt="ZERO-CUBE+BOX SKYBALCONY間取り1F"><span class="ffc">1F</span></li>
+						<li class="hlg01"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_eveplan02.jpg" alt="ZERO-CUBE+BOX SKYBALCONY間取り2F"><span class="ffc">2F</span></li>
+						<li class="hlg01"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_eveplan03.jpg" alt="ZERO-CUBE+BOX SKYBALCONY間取り3F"><span class="ffc">3F</span></li>
 						<li>
 							<div class="cf">
 								<div class="eveinfobox">
-									<p class="tit ffc">+BOX SKYBALCONY</p>
+									<p class="tit ffc">+SKY BALCONY</p>
 									<dl class="eveinfo">
-										<dt><P class="tabbox ffb"><span class="size">4LDK</span></P></dt>
-										<dd class="ffa">延床面積／115.09㎡(34.81坪)<br>1F床面積／62.93㎡(19.04坪)<br>2F床面積／48.02㎡(14.52坪)<br>SKY BALCONY建物内／4.14㎡(1.25坪)<br>SKY BALCONY／46.37㎡(14.01坪)</dd>
+										<dt><P class="tabbox ffb"><span class="size">3LDK</span></P></dt>
+										<dd class="ffa">延床面積／105.15㎡（31.80坪）<br>1F床面積／52.99㎡（16.03坪）<br>2F床面積／48.02㎡（14.52坪）<br>SKY BALCONY建物内／4.14㎡(1.25坪)<br>SKY BALCONY／46.37㎡(14.01坪)</dd>
 									</dl>
 								</div>
 							</div>
@@ -86,16 +86,15 @@
 			<div class="gallery">
 				<div class="under_contents">
 					<ul class="cf">
-						<li><a href="#item01"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall01_s.jpg" alt=""></a></li>
-						<li><a href="#item02"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall02_s.jpg" alt=""></a></li>
-						<li><a href="#item03"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall03_s.jpg" alt=""></a></li>
-						<li><a href="#item04"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall04_s.jpg" alt=""></a></li>
-						<li><a href="#item05"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall05_s.jpg" alt=""></a></li>
-						<li><a href="#item06"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall06_s.jpg" alt=""></a></li>
-						<li><a href="#item07"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall07_s.jpg" alt=""></a></li>
-						<li><a href="#item08"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall08_s.jpg" alt=""></a></li>
-						<li><a href="#item09"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall09_s.jpg" alt=""></a></li>
-						<li><a href="#item10"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall10_s.jpg" alt=""></a></li>
+						<li><a href="#item01"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall01.jpg" alt=""></a></li>
+						<li><a href="#item02"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall02.jpg" alt=""></a></li>
+						<li><a href="#item03"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall03.jpg" alt=""></a></li>
+						<li><a href="#item04"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall04.jpg" alt=""></a></li>
+						<li><a href="#item05"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall05.jpg" alt=""></a></li>
+						<li><a href="#item06"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall06.jpg" alt=""></a></li>
+						<li><a href="#item07"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall07.jpg" alt=""></a></li>
+						<li><a href="#item08"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall08.jpg" alt=""></a></li>
+						<li><a href="#item09"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall09.jpg" alt=""></a></li>
 					</ul>
 				</div>
 				<!-- Item-->
@@ -103,83 +102,74 @@
 					<div class="under_contents">
 						<a href="#" class="close"></a>
 						<h5 class="tit ffc">ZERO-CUBE+FUN</h5>
-						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall01_l.gif" alt="ZERO-CUBE+FUNロゴ"></p>
+						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall01.jpg" alt="ZERO-CUBE+FUN"></p>
 						<p class="txt">ムダを削ぎ落としたキューブ型の美しい外観、服を選ぶようにスタイルに合わせて+FUNを選ぶ住まいの新しいカタチ。</p>
 					</div>
 				</div>
 				<div id="item02" class="port">
 					<div class="under_contents">
 						<a href="#" class="close"></a>
-						<h5 class="tit ffc">LIVING/DINING</h5>
-						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall02_l.jpg" alt="千葉県千葉市稲毛区黒砂モデルハウスZERO-CUBE+BOX SKYBALCONYダイニング"></p>
-						<p class="txt">大きく取られた窓から入りこむ、柔らかな日差しと心地よい風。家族の語らいと笑顔がたくさん生まれる、明るく快適な空間です。</p>
+						<h5 class="tit ffc">SKY BALCONY</h5>
+						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall02.jpg" alt="SKY BALCONY"></p>
+						<p class="txt">空とつながる、もうひとつのリビング。周囲の視線を気にせず、空の下でくつろぐ贅沢なひとときを。</p>
 					</div>
 				</div>
 				<div id="item03" class="port">
 					<div class="under_contents">
 						<a href="#" class="close"></a>
-						<h5 class="tit ffc">JAPANESE ROOM</h5>
-						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall03_l.jpg" alt="千葉県千葉市稲毛区黒砂モデルハウスZERO-CUBE+BOX SKYBALCONY和室"></p>
-						<p class="txt">1Fの和室は、寝室やゲストルーム、リビングの一部として機能。<br>フリールームの変更も可能ですので、趣味のお部屋としてなど+BOXの楽しみ方は自由自在。</p>
+						<h5 class="tit ffc">HALL</h5>
+						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall03.jpg" alt="HALL"></p>
+						<p class="txt">玄関を入るとレッドシダーの板張りがお出迎えブラックのドアがその先のワクワク感を引き立たせます。</p>
 					</div>
 				</div>
 				<div id="item04" class="port">
 					<div class="under_contents">
 						<a href="#" class="close"></a>
-						<h5 class="tit ffc">KITCHEN</h5>
-						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall04_l.jpg" alt="千葉県千葉市稲毛区黒砂モデルハウスZERO-CUBE+BOX SKYBALCONYキッチン"></p>
-						<p class="txt">家族の気配をいつでも感じられる対面キッチン。スペースも広々として使い勝手のいいキッチンレイアウトです。</p>
+						<h5 class="tit ffc">LIVING</h5>
+						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall04.jpg" alt="LIVING"></p>
+						<p class="txt">光と素材が調和し、木の香りと光が満ちる、毎日が特別になるリビング。</p>
 					</div>
 				</div>
 				<div id="item05" class="port">
 					<div class="under_contents">
 						<a href="#" class="close"></a>
-						<h5 class="tit ffc">BED ROOM</h5>
-						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall05_l.jpg" alt="千葉県千葉市稲毛区黒砂モデルハウスZERO-CUBE+BOX SKYBALCONYベッドルーム"></p>
-						<p class="txt">目覚めの朝に、自然光がたっぷりと降り注ぐ、大きな窓が特徴です。</p>
+						<h5 class="tit ffc">DINING</h5>
+						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall05.jpg" alt="DINING"></p>
+						<p class="txt">大きく取られた窓から入りこむ、柔らかな日差しと心地よい風。家族の笑顔が集まる、ぬくもりあふれる空間。</p>
 					</div>
 				</div>
 				<div id="item06" class="port">
 					<div class="under_contents">
 						<a href="#" class="close"></a>
-						<h5 class="tit ffc">ROOM</h5>
-						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall06_l.jpg" alt="千葉県千葉市稲毛区黒砂モデルハウスZERO-CUBE+BOX SKYBALCONYルーム"></p>
-						<p class="txt">一面だけ壁紙を変えてリズムを生み出す空間になっています。</p>
+						<h5 class="tit ffc">KITCHEN</h5>
+						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall06.jpg" alt="KITCHEN"></p>
+						<p class="txt">料理をしながら会話が弾む、心がつながる対面キッチン。スペースも広々として使い勝手のいいキッチンレイアウトです。</p>
 					</div>
 				</div>
 				<div id="item07" class="port">
 					<div class="under_contents">
 						<a href="#" class="close"></a>
-						<h5 class="tit ffc">EXTERIOR</h5>
-						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall07_l.jpg" alt="千葉県千葉市稲毛区黒砂モデルハウスZERO-CUBE+BOX SKYBALCONY外観"></p>
-						<p class="txt">キューブ型のシンプルな外観は周囲からひと際目立ち、ガルバリムのCOOLさ、木の温かみを感じていただけます。</p>
+						<h5 class="tit ffc">POWDER ROOM</h5>
+						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall07.jpg" alt="POWDER ROOM"></p>
+						<p class="txt">木の温もりを感じる造作洗面で、毎日の支度も特別に。</p>
 					</div>
 				</div>
 				<div id="item08" class="port">
 					<div class="under_contents">
 						<a href="#" class="close"></a>
-						<h5 class="tit ffc">WOOD DECK</h5>
-						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall08_l.jpg" alt="千葉県千葉市稲毛区黒砂モデルハウスZERO-CUBE+BOX SKYBALCONYウッドデッキバルコニー"></p>
-						<p class="txt">リビングや和室(フリールームも可)から出入りできるウッドデッキは、BBQも楽しめるアウトドアリビングとして活用できます。</p>
+						<h5 class="tit ffc">ROOM 1</h5>
+						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall08.jpg" alt="ROOM 1"></p>
+						<p class="txt">テントの中で夢を描くように、子どもたちの想像力が育つ場所。遊び心を詰め込んだ、小さなグランピングルーム。</p>
 					</div>
 				</div>
 				<div id="item09" class="port">
 					<div class="under_contents">
 						<a href="#" class="close"></a>
-						<h5 class="tit ffc">SKY BALCONY - DAYTIME -</h5>
-						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall09_l.jpg" alt="千葉県千葉市稲毛区黒砂モデルハウスZERO-CUBE+BOX SKYBALCONYスカイバルコニー"></p>
-						<p class="txt">プライバシーを保ちつつも、開放感ある贅沢なくつろぎスペース。家族や仲間と一緒にベランピングライフを満喫できます。</p>
+						<h5 class="tit ffc">MAIN BED ROOM</h5>
+						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/zero-cube-muji-img_gall09.jpg" alt="MAIN BED ROOM"></p>
+						<p class="txt">グレージュの優しい色合いが、一日の終わりを穏やかに整える、やすらぎをデザインした寝室。</p>
 					</div>
 				</div>
-				<div id="item10" class="port">
-					<div class="under_contents">
-						<a href="#" class="close"></a>
-						<h5 class="tit ffc">SKY BALCONY - NIGHT TIME -</h5>
-						<p class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/event/img_gall10_l.jpg" alt="千葉県千葉市稲毛区黒砂モデルハウスZERO-CUBE+BOX SKYBALCONYスカイバルコニー"></p>
-						<p class="txt">夜はライトアップでラグジュアリーな空間を演出。リゾート地にいるような非日常なくつろぎを体験できます。</p>
-					</div>
-				</div>
-			</div>
 		</div>
 <?php require("inc/event-comment01.php"); ?>
 		
